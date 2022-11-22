3c582b5ccbe575963b8af3f5ab9834595175998a
Thair <110577068+Thair-i@users.noreply.github.com>
2022-11-21 19:43:00 -0800

    first commit

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-const sequelize = require('./config/connection');
+//const helpers = require('./utils/helpers');
+//const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Create the Handlebars.js engine object with custom helper functions
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 // Inform Express.js which template engine we're using
 app.engine('handlebars', hbs.engine);
@@ -21,6 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+//sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-});
+//});
