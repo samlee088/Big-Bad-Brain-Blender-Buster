af7e866ec893c8f0b3e6af0aba60fb6c1ff6803c
Thair <110577068+Thair-i@users.noreply.github.com>
2022-11-24 00:01:05 -0800

    add the home and signin routes plus making handlebars better with some CSS

--- controllers/signupRouter.js
+++ controllers/signupRouter.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 //const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const signUp = require('./signup');
 
-router.use('/', homeRoutes);
+router.use('/', signup);
 //router.use('/api', apiRoutes);
 
 module.exports = router;
