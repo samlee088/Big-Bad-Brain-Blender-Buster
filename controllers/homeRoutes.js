8a0f05f1899ffd4d531557eb6662af9155829149
samlee088 <108655429+samlee088@users.noreply.github.com>
2022-11-25 18:27:32 -0800

    quiz rendering, able to cycle through questions, able to view results page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,7 +10,7 @@ router.get('/login', async (req, res) => {
   res.render('login');
 });
 
-router.get('/sigup', async (req, res) => {
+router.get('/signup', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
   res.render('signup');
 });
