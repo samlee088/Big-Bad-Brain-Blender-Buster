bcf7fea9f307130f60dc2bd3978c34cfdba93260
samlee088 <108655429+samlee088@users.noreply.github.com>
2022-11-30 18:32:48 -0800

    heroku troubleshoot router

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(routes);
+app.use("/routes", routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
